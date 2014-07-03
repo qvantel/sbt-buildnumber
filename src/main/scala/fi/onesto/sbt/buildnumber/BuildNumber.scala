389d11da8be705067a24d753a92113c48b09ecf9
Olli Helenius <liff@iki.fi>
2014-07-03 21:45:29 +0300

    Slight refactoring and small changes
      - Add shortBuildNumber
      - Add decoratedShortBuildNumber
      - Add branchName
      - Run git commands in baseDirectory

--- src/main/scala/fi/onesto/sbt/buildnumber/BuildNumber.scala
+++ src/main/scala/fi/onesto/sbt/buildnumber/BuildNumber.scala
@@ -5,26 +5,67 @@ import sbt._
 
 
 object BuildNumber extends Plugin {
-  val unstagedChanges      = taskKey[Boolean]("true if the current repository has unstaged changes")
-  val uncommittedChanges   = taskKey[Boolean]("true if the current repository has uncommitted changes")
-  val untrackedFiles       = taskKey[Seq[File]]("list of files that are not in version control")
-  val buildNumber          = taskKey[Option[String]]("current revision of the repository")
-  val decoratedBuildNumber = taskKey[Option[String]]("current revision with decorations")
+  import sbt.Keys.baseDirectory
+
+  private[this] object C {
+    val Unstaged    = "*"
+    val Untracked   = "%"
+    val Uncommitted = "+"
+  }
+
+  private[this] object Git {
+    val GetUnstaged         = "git diff --no-ext-diff --quiet --exit-code"
+    val GetUncommitted      = "git diff-index --cached --quiet HEAD"
+    val GetUntracked        = "git ls-files --others --exclude-standard"
+    val GetBuildNumber      = "git rev-parse --quiet --verify HEAD"
+    val GetShortBuildNumber = "git rev-parse --quiet --verify --short HEAD"
+    val GetBranchName       = "git rev-parse --quiet --verify --abbrev-ref HEAD"
+  }
+
+  val unstagedChanges           = taskKey[Boolean]("true if the current repository has unstaged changes")
+  val uncommittedChanges        = taskKey[Boolean]("true if the current repository has uncommitted changes")
+  val untrackedFiles            = taskKey[Seq[File]]("list of files that are not in version control")
+  val buildNumber               = taskKey[Option[String]]("current revision of the repository")
+  val shortBuildNumber          = taskKey[Option[String]]("short version of the current revision of the repository")
+  val branchName                = taskKey[Option[String]]("the current branch name")
+  val decoratedBuildNumber      = taskKey[Option[String]]("current revision with decorations")
+  val decoratedShortBuildNumber = taskKey[Option[String]]("current revision with decorations")
+
+  private[this] implicit class BooleanToMark(val underlying: Boolean) extends AnyVal {
+    def toMark(mark: String): String = if (underlying) mark else ""
+  }
+
+  private[this] def boolCommand(in: File, command: String): Boolean =
+    Try(Process(command, in).! != 0) getOrElse false
+  private[this] def linesCommand(in: File, command: String): Seq[String] =
+    Try(Process(command, in).lines_!.toVector) getOrElse Vector.empty
+  private[this] def firstLineCommand(in: File, command: String): Option[String] =
+    linesCommand(in, command).headOption
+  private[this] def filesCommand(in: File, command: String): Seq[File] =
+    linesCommand(in, command).map(file)
 
   val buildNumberSettings = Seq(
-    unstagedChanges      := Try("git diff --no-ext-diff --quiet --exit-code".! != 0) getOrElse false,
-
-    untrackedFiles       := Try("git ls-files --others --exclude-standard".lines_!.map(file)) getOrElse Seq.empty,
-
-    uncommittedChanges   := Try("git diff-index --cached --quiet HEAD".! != 0) getOrElse false,
-
-    buildNumber          := Try("git rev-parse --quiet --verify HEAD".lines_!.headOption) getOrElse None,
+    unstagedChanges    := boolCommand(baseDirectory.value, Git.GetUnstaged),
+    uncommittedChanges := boolCommand(baseDirectory.value, Git.GetUncommitted),
+    untrackedFiles     := filesCommand(baseDirectory.value, Git.GetUntracked),
+    buildNumber        := firstLineCommand(baseDirectory.value, Git.GetBuildNumber),
+    shortBuildNumber   := firstLineCommand(baseDirectory.value, Git.GetShortBuildNumber),
+    branchName         := firstLineCommand(baseDirectory.value, Git.GetBranchName),
 
     decoratedBuildNumber := {
       buildNumber.value map { revision =>
-        val unstagedMark    = if (unstagedChanges.value)        "*" else ""
-        val untrackedMark   = if (untrackedFiles.value.isEmpty) "%" else ""
-        val uncommittedMark = if (uncommittedChanges.value)     "+" else ""
+        val unstagedMark    = (unstagedChanges.value).toMark(C.Unstaged)
+        val untrackedMark   = (untrackedFiles.value.nonEmpty).toMark(C.Untracked)
+        val uncommittedMark = (uncommittedChanges.value).toMark(C.Uncommitted)
+        s"$revision$unstagedMark$untrackedMark$uncommittedMark"
+      }
+    },
+
+    decoratedShortBuildNumber := {
+      shortBuildNumber.value map { revision =>
+        val unstagedMark    = (unstagedChanges.value).toMark(C.Unstaged)
+        val untrackedMark   = (untrackedFiles.value.nonEmpty).toMark(C.Untracked)
+        val uncommittedMark = (uncommittedChanges.value).toMark(C.Uncommitted)
         s"$revision$unstagedMark$untrackedMark$uncommittedMark"
       }
     }
