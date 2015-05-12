123a3233207b6cc7cf032a40531eadd5185d3a20
Olli Helenius <olli.helenius@onesto.fi>
2015-05-12 16:37:59 +0300

    Move keys to internal Keys object

--- src/main/scala/fi/onesto/sbt/buildnumber/BuildNumber.scala
+++ src/main/scala/fi/onesto/sbt/buildnumber/BuildNumber.scala
@@ -7,17 +7,21 @@ object BuildNumber extends AutoPlugin {
   import Helpers._
   import sbt.Keys.baseDirectory
 
-  val scmType                   = taskKey[Scm]("detected type of version control")
-  val unstagedChanges           = taskKey[Boolean]("true if the current repository has unstaged changes")
-  val uncommittedChanges        = taskKey[Boolean]("true if the current repository has uncommitted changes")
-  val untrackedFiles            = taskKey[Seq[File]]("list of files that are not in version control")
-  val buildNumber               = taskKey[Option[String]]("current revision of the repository")
-  val shortBuildNumber          = taskKey[Option[String]]("short version of the current revision of the repository")
-  val branchName                = taskKey[Option[String]]("the current branch name")
-  val decoratedBuildNumber      = taskKey[Option[String]]("current revision with decorations")
-  val decoratedShortBuildNumber = taskKey[Option[String]]("short version of the current revision with decorations")
+  object Keys {
+    val scmType                   = taskKey[Scm]("detected type of version control")
+    val unstagedChanges           = taskKey[Boolean]("true if the current repository has unstaged changes")
+    val uncommittedChanges        = taskKey[Boolean]("true if the current repository has uncommitted changes")
+    val untrackedFiles            = taskKey[Seq[File]]("list of files that are not in version control")
+    val buildNumber               = taskKey[Option[String]]("current revision of the repository")
+    val shortBuildNumber          = taskKey[Option[String]]("short version of the current revision of the repository")
+    val branchName                = taskKey[Option[String]]("the current branch name")
+    val decoratedBuildNumber      = taskKey[Option[String]]("current revision with decorations")
+    val decoratedShortBuildNumber = taskKey[Option[String]]("short version of the current revision with decorations")
+  }
 
   override val trigger = allRequirements
+
+  import Keys._
 
   override val projectSettings = Seq(
     scmType := Scm.detect((baseDirectory in LocalRootProject).value),
